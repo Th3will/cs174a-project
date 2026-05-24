bfe41e8a7db920d6c5ecd67d001f9238eb69a50f
Jason Wang <jasonw123451@gmail.com>
2026-05-24 10:49:32 -0700

    getters for replenishment orders and shipping notice lines

--- project/src/main/java/eDepot/dao/NoticeLineDAO.java
+++ project/src/main/java/eDepot/dao/NoticeLineDAO.java
@@ -6,6 +6,10 @@ import eDepot.utils.DatabaseConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class NoticeLineDAO {
     public boolean insertNoticeLine(NoticeLine line) {
@@ -30,5 +34,31 @@ public class NoticeLineDAO {
         return false;
     }
 
-    // TODO: add methods to list all lines by shipping notice ID.
+    // return all rows in a shipping notice that correspond to a specific shipping notice ID
+    public List<NoticeLine> getLinesForNotice(int snid) {
+        List<NoticeLine> noticeLines = new ArrayList<>();
+        String sql = "SELECT snid, stock_num, notice_quantity FROM eDepot_Notice_Line WHERE snid = ?";
+
+        try (Connection conn = DatabaseConnection.testConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, snid);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    int row_snid = rs.getInt("snid");
+                    String row_stock_num = rs.getString("stock_num");
+                    int row_notice_quantity = rs.getInt("notice_quantity");
+
+                    NoticeLine line = new NoticeLine(row_snid, row_stock_num, row_notice_quantity);
+                    noticeLines.add(line);
+                }
+            }
+            return noticeLines;
+        }
+        catch (SQLException e) {
+            System.err.println("DB error while fetching shipping notice line: " + e.getMessage());
+        }
+        return noticeLines;
+    }
 }
--- project/src/main/java/eDepot/dao/ReplenishmentLineDAO.java
+++ project/src/main/java/eDepot/dao/ReplenishmentLineDAO.java
@@ -6,6 +6,10 @@ import eDepot.utils.DatabaseConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class ReplenishmentLineDAO {
     public boolean insertReplenishmentLine(ReplenishmentLine line) {
@@ -30,5 +34,31 @@ public class ReplenishmentLineDAO {
         return false;
     }
 
-    // TODO: add methods to list all lines by replenishment order ID.
+    // return all lines in a replenishment order corresponding to a specific replenishment order ID
+    public List<ReplenishmentLine> getLinesForReplenishmentOrder(int oid) {
+        List<ReplenishmentLine> replenishmentLines = new ArrayList<>();
+        String sql = "SELECT oid, stock_num, replenishment_quantity FROM eDepot_Replenishment_Line WHERE oid = ?";
+
+        try (Connection conn = DatabaseConnection.testConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, oid);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    int row_oid = rs.getInt("oid");
+                    String row_stock_num = rs.getString("stock_num");
+                    int row_replenishment_quantity = rs.getInt("replenishment_quantity");
+
+                    ReplenishmentLine line = new ReplenishmentLine(row_oid, row_stock_num, row_replenishment_quantity);
+                    replenishmentLines.add(line);
+                }
+            }
+            return replenishmentLines;
+        }
+        catch (SQLException e) {
+            System.err.println("DB error while fetching shipping notice line: " + e.getMessage());
+        }
+        return replenishmentLines;
+    }
 }
