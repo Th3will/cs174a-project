daa1fe5a95164a54b0cf81d04347e2ad7c6b93f9
william ni <williamni@ucsb.edu>
2026-05-23 17:12:19 -0700

    scaffolding for User Interface

---
+++ project/src/main/java/eMart/UserInterface.java
@@ -0,0 +1,27 @@
+package eMart;
+
+public class UserInterface {
+    
+
+    public interface access{
+        public static void login(){
+
+        }
+        public static void create_new_user(){
+
+        }
+
+    }
+
+    public interface entity_listing {
+        
+    }
+
+    public static void run(){
+        // user selects 1. login or 2. create new user
+        // the will create a user (eventually either a customer or an employee)
+        // customer interface will then 
+        // 
+    }
+}
+
